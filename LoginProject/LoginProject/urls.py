45f92b139e89e3df72b3711ac0f4787c66f2bd07
sharan122 <sharanraj037840@gmail.com>
2024-07-17 20:27:29 +0530

    second commit

--- LoginProject/LoginProject/urls.py
+++ LoginProject/LoginProject/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('home/',views.home,name="home"),
     path('logout/',views.logoutpage,name="logout"),
     path('admin/', admin.site.urls),
+    path('admins/',views.admins,name="admins"),
+    path('add/',views.add,name="add"),
+    path('update/<str:id>/',views.update,name="update"),
+    path('delete/<str:id>/',views.delete,name="delete")
 ]
--- LoginProject/home/views.py
+++ LoginProject/home/views.py
@@ -2,34 +2,49 @@ from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.cache import cache_control
+from django.views.decorators.cache import cache_control,never_cache
 from django.contrib import messages
 # Create your views here.
 
 @cache_control(no_cache=True, must_revalidate=True, no_store=True)
 def loginPage(request):
+   
     if request.session.session_key:
-        return redirect('home')
+        if request.user.is_staff:
+                
+            return redirect('admins')
+        else:
+    
+            return redirect('home')
+
     if request.method=='POST':
         username=request.POST.get('username')
         pass1=request.POST.get('pass')
         user=authenticate(request,username=username,password=pass1)
+        
         if user is not None:
             login(request,user)
+            if request.user.is_staff:
+                
+                return redirect('admins')
+    
             return redirect(home)
         elif not username or not pass1:
             messages.error(request,"Can'nt Leave Username or Password Empty")
-            return redirect(loginPage)
+            return redirect(loginPage)  
         else:
             messages.error(request,"Check your Username Or Password")
             return redirect(loginPage)
         
     return render(request, 'login.html')
 
-
+@never_cache
 def signupPage(request):
-    if request.user.is_authenticated:
-        return redirect('home')
+    if request.session.session_key:
+        if request.user.is_staff:
+            return redirect('admins')
+        else:
+            return redirect('home')
   
     if request.method=='POST':
         username=request.POST.get('username')
@@ -42,16 +57,18 @@ def signupPage(request):
         elif not username or not email or not pass1 or not pass2:
             messages.error(request,"Can'nt Leave Any Feild  Empty")
             return redirect(signupPage)
-        else:
+        else:   
             my_user=User.objects.create_user(username,email,pass1)
             my_user.save()
             return redirect('loginPage')
             
     return render(request, 'signup.html')
 
-@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+@never_cache
 @login_required(login_url='loginPage')
 def home(request):
+    if request.user.is_superuser:
+        return redirect('admins')
    
     return render(request,'home.html')
 
@@ -59,4 +76,55 @@ def logoutpage(request):
     logout(request)
     return redirect('loginPage')
 
+
+@login_required(login_url='loginPage')
+@never_cache
+def admins(request):
+    if not request.user.is_superuser:
+        return redirect('home')
+    else:
+        txt=User.objects.filter(is_superuser=False)
+        if request.method=='GET':
+            username=request.GET.get('q','')
+            txt=User.objects.filter(username__icontains=username,is_superuser=False)
+           
+        return render(request,'admin.html',{'txt':txt})
+            
+            
+            
+        
+         
+    
+@login_required(login_url='loginPage') 
+@never_cache   
+def add(request):
+    if request.method == 'POST':
+        name=request.POST.get('name1')
+        email=request.POST.get('email1')
+        newuser=User.objects.create_user(name,email)
+        newuser.save()
+        return redirect('admins')
+    return render(request,'admin.html')
+@login_required(login_url='loginPage')
+@never_cache
+def edit(request):
+    
+    return render(request,'admin.html')
+
+@login_required(login_url='loginPage')
+@never_cache
+def update(request,id):
+    if request.method == 'POST':
+        name=request.POST.get('name2')
+        email=request.POST.get('email2')
+        newuser2=User(id=id,username=name,email=email)
+        newuser2.save()
+        return redirect('admins')
+    return redirect(request,'admins')
+
+@login_required(login_url='loginPage')  
+@never_cache 
+def delete(request,id):
+    txt=User.objects.filter(id=id).delete()
+    return redirect('admins')
     
